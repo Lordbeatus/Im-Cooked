3f6c1e4bbb09f6b78e1e6a38574a236d9a7de132
Daniel Thomas <155140065+Lordbeatus@users.noreply.github.com>
2025-01-21 09:48:12 -0800

    Update timer.js

--- timer.js
+++ timer.js
@@ -56,36 +56,6 @@ document.getElementById('start-timer').addEventListener('click', () => {
 
 
 
-//Countdown timer logic
-
-// Calling defined function at certain interval
-let x = setInterval(function () {
-
-  // Calculating difference
-  let t = minutes;
-
-  // Getting values of days,hours,minutes, seconds
-  let days = Math.floor(t / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((t % (1000 * 60)) / 1000);
-
-  // Show the output time
-  document.getElementById("day").innerHTML = days;
-  document.getElementById("hour").innerHTML = hours;
-  document.getElementById("minute").innerHTML = minutes;
-  document.getElementById("second").innerHTML = seconds;
-
-  // Show overtime output
-  if (t < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "TIME UP";
-    document.getElementById("day").innerHTML = "0";
-    document.getElementById("hour").innerHTML = "0";
-    document.getElementById("minute").innerHTML = "0";
-    document.getElementById("second").innerHTML = "0";
-  }
-}, 1000);
 
 
 
